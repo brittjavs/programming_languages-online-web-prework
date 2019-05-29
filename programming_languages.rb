29d8cf12e63c7eca123604bb1dbf60c7a88716bf
Brittany Javalera <brittjavalera@gmail.com>
2019-05-29 18:14:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -18,7 +18,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |lang_style, lang_names|
     lang_names.each do |names, types|
-    new_hash[names] = types
+    new_hash[names] ||= types
     new_hash[names][:style] = [lang_style]
     end
   end
